df0281952537066b04b339b8ad1ec43205bd9ddf
sameer-shaikh <sameermubeen4@gmail.com>
2021-08-27 14:28:22 +0500

    language data done

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -129,14 +129,258 @@ export const data = [
 		},
 		DE: {
 			home: {
+				// HERO START
 				hero: "Es geht nur um deine erfahrung",
 				hero2: "VOLLBILD-VIDEO ALLGEMEIN KURZ AUTOPLAY KEIN TON",
+				heroBtn: "Gehe zu Shows",
+				// HERO END
+
+				// ABOUT START
+				aboutH: "Über uns",
+				aboutL:
+					"UshowMe ist eine interaktive Live-Stream- und Video-on-Demand-Plattform, die Künstlern, Veranstaltungsproduzenten wie Veranstaltungsorten und Festivals dabei hilft, sich zu hybriden Shows mit globaler Reichweite zu entwickeln, neue Erlebnisse, mehrere Einnahmequellen zu bieten und Marken und digitales Publikum neue Möglichkeiten der Interaktion zu bieten .",
+				aboutR1: "GEBÄUDE FÜR JEDE MODERNE GERÄTE",
+				aboutR2:
+					"Das reaktionsschnelle Design von UshowMe ist darauf zugeschnitten, Ihrem Publikum die besten Erfahrungen und Interaktionen zu bieten, unabhängig davon, welches moderne Gerät es verwendet.",
+				aboutR3: "siehe Liste der unterstützten Geräte auf der FAQ-Seite.",
+				// ABOUT END
+
+				// HYBRID START
+				hybH: "Warum Hybrid die Zukunft ist?",
+				hybP: [
+					"Bei Shows geht es nicht nur darum, mit deinem „Tribe“ diese Lieblingssongs zu singen und mit den Künstlern zu interagieren, sondern darum, gemeinsame Erfahrungen zu leben und unsere Emotionen auszudrücken, um lang anhaltende Erinnerungen an eine großartige Zeit zusammen zu schaffen.",
+					"Hybrid verstärkt diese und beseitigt die üblichen Einschränkungen, die viele Fans auslassen, während es mehr Einnahmequellen und neues messbares Engagement hinzufügt.",
+					"UshowMe ist modular aufgebaut, um Ihre spezifischen Anforderungen zu maximieren.",
+					"Beginnen Sie, indem Sie unten auswählen, wer U ist.",
+				],
+				// HYBRID END
+
+				// SIDEBAR START
+				sideBar: [
+					{ t: "ÜBER UNS" },
+					{ t: "AUSGEWÄHLTE SHOWS" },
+					{ t: "HYBRID IST DIE ZUKUNFT" },
+					{ t: "WIE ES FUNKTIONIERT" },
+					{ t: "WER BIST DU?" },
+					{ t: "FAQ" },
+				],
+				// SIDEBAR END
+
+				// WHO START
+				who1: "BEGINNEN SIE MIT DER AUSWAHL",
+				who2: "Wer bist du?",
+				// WHO END
+
+				// FEATURE START
+				feature1: "Angesagte Shows",
+				feature2: "Alle Shows anzeigen",
+				// FEATURE END
+
+				// FEATURE START
+				itsAll1: "Alles dreht sich um SPASS!",
+				itsAll2: "Vollständiges Video anzeigen",
+				// FEATURE END
+
+				// NEWS START
+				news: "In den Nachrichten",
+				// NEWS END
+
+				// PARTNER START
+				partner1: "PARTNER",
+				partner2: "Ein Partner werden",
+				partner3: "Führen Sie die Zukunft für Hybridshows.",
+				// PARTNER END
+
+				// PARTNER START
+				newsletter1: "Abonniere den Newsletter",
+				newsletterBtn: "Senden",
+				// PARTNER END
+
+				// WHO END
+				who1: "ich bin ein",
+				present: "Holen Sie sich die Präsentation:",
+				send: "Senden",
+				slide1T: "Künstler/ Label",
+				slide1T2:
+					"Erreichen Sie ein globales Publikum und interagieren Sie mit ihm",
+				slide1D:
+					"Sie müssen Ihre Fans nie wieder draußen lassen. Mit UshowMe können Sie sich darauf verlassen, dass Sie alle einbeziehen und mit ihnen interagieren, den Umsatz über mehrere Streams sowohl für die Live-Stream- als auch für die Video-on-Demand-Version steigern und auch Ihre eigenen Sponsoren an Bord holen.",
+				slide2T: "Produzent/ Veranstaltungsort",
+				slide2T2:
+					"Skalieren Sie Shows über Ihren Veranstaltungsort/Standort hinaus",
+				slide2D:
+					"Mit UshowMe können Sie sich als Produzent und/oder Veranstaltungsort zu einem Hybrid entwickeln und Ihr Publikum über physische Grenzen hinaus erreichen, mit mehreren Streams Geld verdienen und bereits auf Ihrem physischen Veranstaltungsort/Festival präsente Sponsoren digitalisieren.",
+				slide3T: "Marke",
+				slide3T2: "Sponsern Sie Shows und messen Sie Engagements",
+				slide3D:
+					"Ihre Marke hat endlich die Macht, die Bekanntheit zu maximieren und neue Wege der Interaktion mit dem digitalen Publikum zu eröffnen. UshowMe bietet Ihnen Tools für Ihre Marke, um mit messbaren Ergebnissen das Highlight zu erzielen, das sie verdient. UshowMe bietet vollständige Integrationsmöglichkeiten.",
+				slide4T: "Fan",
+				slide4T2: "Viel Spaß mit neuen Interaktionen",
+				slide4D:
+					"Mit UshowMe haben Sie mehr Zugang zu Ihren Lieblingskünstlern, als wenn Sie persönlich vor Ort wären. Schließen Sie Ihre Kamera an, drücken Sie Emotionen und Ihre besten Moves auf der Top Fans Stage aus. Sprechen Sie mit allen im Chat und tauschen Sie sich mit Künstlern bei aufregenden Meet & Greets aus.",
+				newLet: "Abonniere den Newsletter:",
+				// WHO END
+
+				// FOOTER START
+				footer: [
+					{
+						t: "Wie es funktioniert",
+						l: "",
+					},
+					{
+						t: "FAQ",
+						l: "",
+					},
+					{
+						t: "Arbeitsplätze",
+						l: "",
+					},
+					{
+						t: "Kontaktiere uns",
+						l: "",
+					},
+					{
+						t: "Medien",
+						l: "",
+					},
+					{
+						t: "Datenschutz-Bestimmungen",
+						l: "",
+					},
+					{
+						t: "Terms & amp; Bedingungen",
+						l: "",
+					},
+				],
+				footerBtn: "Alle Shows anzeigen",
+				// FOOTER END
 			},
 		},
 		PT: {
 			home: {
+				// HERO START
 				hero: "É tudo sobre a sua experiência",
 				hero2: "FULL SCREEN VIDEO GERAL CURTO AUTOPLAY SEM SOM",
+				heroBtn: "Ir para shows",
+				// HERO END
+
+				// ABOUT START
+				aboutH: "Sobre nós",
+				aboutL:
+					"UshowMe é uma plataforma interativa de transmissão ao vivo e vídeo sob demanda para ajudar artistas, produtores de eventos, como locais e festivais, a evoluir para shows híbridos com alcance global, oferecer novas experiências, múltiplos fluxos de receita e dar a marcas e públicos digitais novas formas de engajamento .",
+				aboutR1: "CONSTRUÇÃO PARA QUALQUER DISPOSITIVO MODERNO",
+				aboutR2:
+					"O design responsivo do UshowMe é adaptado para oferecer as melhores experiências e interações ao seu público, independentemente do dispositivo moderno que eles usam.",
+				aboutR3: "veja a lista de dispositivos suportados na página de FAQ.",
+				// ABOUT END
+
+				// HYBRID START
+				hybH: "Por que híbrido é o futuro?",
+				hybP: [
+					"Mais do que cantar aquelas músicas favoritas com sua “Tribo” e interagir com os artistas, os shows são sobre viver experiências compartilhadas e expressar nossas emoções, criando memórias duradouras de bons momentos juntos.",
+					"O Hybrid amplifica isso e remove as limitações de tijolo e argamassa que deixam muitos ventiladores de fora, enquanto adiciona mais fontes de receita e um novo envolvimento que pode ser medido.",
+					"UshowMe é construído de forma modular para maximizar suas necessidades específicas.",
+					"Comece selecionando quem é você abaixo.",
+				],
+				// HYBRID END
+
+				// SIDEBAR START
+				sideBar: [
+					{ t: "SOBRE NÓS" },
+					{ t: "MOSTRA EM DESTAQUE" },
+					{ t: "HÍBRIDO É O FUTURO" },
+					{ t: "COMO FUNCIONA" },
+					{ t: "QUEM É VOCÊ?" },
+					{ t: "Perguntas frequentes" },
+				],
+				// SIDEBAR END
+
+				// WHO START
+				who1: "COMECE SELECIONANDO",
+				who2: "Quem é você?",
+				// WHO END
+
+				// FEATURE START
+				feature1: "Programas em Destaque",
+				feature2: "Ver todos os programas",
+				// FEATURE END
+
+				// FEATURE START
+				itsAll1: "É tudo sobre DIVERSÃO!",
+				itsAll2: "Veja o vídeo completo",
+				// FEATURE END
+
+				// NEWS START
+				news: "Nas notícias",
+				// NEWS END
+
+				// PARTNER START
+				partner1: "PARCEIROS",
+				partner2: "Torne-se um parceiro",
+				partner3: "Lidere o futuro para programas híbridos.",
+				// PARTNER END
+
+				// PARTNER START
+				newsletter1: "Assine o Newsletter",
+				newsletterBtn: "Mandar",
+				// PARTNER END
+
+				// WHO END
+				who1: "Eu sou um",
+				present: "Obtenha a apresentação:",
+				send: "Mandar",
+				slide1T: "Artista / gravadora",
+				slide1T2: "Alcance e interaja com públicos globais",
+				slide1D:
+					"Nunca mais terá que deixar seus fãs de fora. Com o UshowMe, conte com a inclusão e interação com todos, aumentando a receita por meio de vários streams para as versões de stream ao vivo e vídeo sob demanda e também traga seus próprios patrocinadores a bordo.",
+				slide2T: "Produtor / Local",
+				slide2T2: "Mostra a escala além do seu local / local",
+				slide2D:
+					"Com o UshowMe, você, como produtor e / ou local, pode evoluir para um híbrido e atingir seu público além das limitações físicas, monetizar com vários streams e trazer patrocinadores já presentes em seu local / festival físico para o digital.",
+				slide3T: "Marca",
+				slide3T2: "Patrocinador mostra e avalia compromissos",
+				slide3D:
+					"Sua marca finalmente tem o poder de maximizar o conhecimento e novas formas de engajamento com o público digital. UshowMe fornece ferramentas para sua marca obter o destaque que merece com resultados mensuráveis. UshowMe oferece recursos de integração total.",
+				slide4T: "Fã",
+				slide4T2: "Divirta-se com novas interações",
+				slide4D:
+					"O UshowMe dá a você mais acesso aos seus artistas favoritos do que se você estivesse lá pessoalmente. Conecte sua câmera, expresse emoções e seus melhores movimentos no Top Fans Stage. Fale com todos no chat e envolva-se com artistas em excitantes Meet & Greets.",
+				newLet: "Assine o Newsletter:",
+				// WHO END
+
+				// FOOTER START
+				footer: [
+					{
+						t: "Como funciona",
+						l: "",
+					},
+					{
+						t: "Perguntas frequentes",
+						l: "",
+					},
+					{
+						t: "Empregos",
+						l: "",
+					},
+					{
+						t: "Entre em contato conosco",
+						l: "",
+					},
+					{
+						t: "meios de comunicação",
+						l: "",
+					},
+					{
+						t: "Política de Privacidade",
+						l: "",
+					},
+					{
+						t: "termos e Condições",
+						l: "",
+					},
+				],
+				footerBtn: "Ver todos os programas",
+				// FOOTER END
 			},
 		},
 	},
