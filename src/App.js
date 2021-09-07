8e04f6a940c4696d03b91dda06af983cf5a8e198
sameer-shaikh <sameermubeen4@gmail.com>
2021-09-08 01:48:32 +0500

    all done

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import "./styles/style.css";
 // IMPORTING LOADER
 import HashLoader from "react-spinners/HashLoader";
 
-// Import css files
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { GlobalProvider } from "./data/Context";
 
 // IMPORTING PAGES
